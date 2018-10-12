8976e41e8163dd8fffe60121910636ebf3356cf3
Carolina Garcia <caro1985garcia@gmail.com>
2018-10-12 01:01:16 -0300

    cambio

--- src/App.js
+++ src/App.js
@@ -9,15 +9,18 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+             
+             Hello React
+
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="http://186.64.118.50/~mpcgarcia/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Vive Quilicura
+
           </a>
         </header>
       </div>
